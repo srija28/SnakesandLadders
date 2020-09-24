f1c61c84ef7eb7a299aeff61997dcc338f3f29c4
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-24 18:32:14 +0530

    Added to uc4

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -10,23 +10,29 @@ public class SnakeAndLadders {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snakes and Ladders Game");
 		System.out.println("Player starting at Position 0");
-		int dice = ((int) Math.floor(Math.random()*10)%6)+1;
-		System.out.println("Player rolls the number:"+dice);
 		
 		int currentPosition = INITIAL_POSITION;
-		int check = (int) Math.floor(Math.random()*10)%3;
-		System.out.println("check is " + check);
+		int move = 0;
 		
-		if(check == NO_PLAY)
-			System.out.println("No Action Taken");
-		else if(check == LADDER)
-			currentPosition += dice;
-		else {
-			currentPosition -= dice;
-			if(currentPosition<INITIAL_POSITION)
-				currentPosition = INITIAL_POSITION;
+		while(currentPosition<100) {
+			int dice = ((int) Math.floor(Math.random()*10)%6)+1;
+			//System.out.println("Player rolls the number:"+dice);
+			int check = (int) Math.floor(Math.random()*10)%3;
+			//System.out.println("check is " + check);
+			
+			if(check == NO_PLAY)
+				currentPosition = currentPosition;
+			else if(check == LADDER)
+				currentPosition += dice;
+			else {
+				currentPosition -= dice;
+				if(currentPosition<INITIAL_POSITION)
+					currentPosition = INITIAL_POSITION;
+			
+				}
+			move+=1;
 		}
-		System.out.println("The player is at :"+currentPosition);
+		System.out.println("The player reaches 100 in "+ move +"moves");
 			
 	}	
 }
