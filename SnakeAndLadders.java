38c57d5af1aad963c5729a18fac9684c45290c05
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-24 17:44:47 +0530

    Added uc1

---
+++ SnakeAndLadders.java
@@ -0,0 +1,10 @@
+package SnakeAndLadders;
+
+public class SnakeAndLadders {
+	public static final int INITIAL_POSITION =0;
+	public static final int NO_OF_PLAYERS =1;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snakes and Ladders Game");
+		System.out.println("Player starting at Position 0");
+	}	
+}
