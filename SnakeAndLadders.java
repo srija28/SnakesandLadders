bf89e3f78b3a46acad206af4be215afe68a639e4
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-09-26 12:29:54 +0530

    A

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -1,13 +1,7 @@
 package SnakeAndLadders;
 
 public class SnakeAndLadders {
-<<<<<<< HEAD
-	public static final int INITIAL_POSITION =0;
-	public static final int NO_OF_PLAYERS =2;
-	public static final int NO_PLAY =0;
-=======
 	public static final int NO_PLAY = 0;
->>>>>>> uc7
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
 	public static int PLAYER1_POSITION = 0;
@@ -20,37 +14,6 @@ public class SnakeAndLadders {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snakes and Ladders Game");
-<<<<<<< HEAD
-		System.out.println("Player starting at Position 0");
-		
-		int currentPosition = INITIAL_POSITION;
-		int move = 0;
-		int count = 0;
-		
-		while(currentPosition != 100) {
-			
-			int dice = ((int) Math.floor(Math.random()*10)%6)+1;
-				count+=1;
-				
-				//System.out.println("Player rolls the number:"+dice);
-			int check = (int) Math.floor(Math.random()*10)%3;
-				//System.out.println("check is " + check);
-				
-			if(check == NO_PLAY)
-				currentPosition = currentPosition;
-				
-			else if(check == LADDER) {
-				currentPosition += dice;
-				if(currentPosition>100)
-						currentPosition-=dice;
-			}
-					
-			else {
-				currentPosition -= dice;
-				if(currentPosition<INITIAL_POSITION)
-					currentPosition = INITIAL_POSITION;
-				
-=======
 		System.out.println("Player 1 and Player 2 start at Position 0");
 		while ((PLAYER1_POSITION < 100) && (PLAYER2_POSITION < 100)) {
 			int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
@@ -73,7 +36,6 @@ public class SnakeAndLadders {
 						PLAYER1_POSITION = 0;
 					PLAYER1_TURN = 0;
 					PLAYER2_TURN = 2;
->>>>>>> uc7
 				}
 				System.out.println("The position of player 1 is: " + PLAYER1_POSITION);
 			} else if (PLAYER2_TURN == 2) {
@@ -104,18 +66,5 @@ public class SnakeAndLadders {
 		} else {
 			System.out.println("Player 2 won and his dice roll count is :" + PLAYER2_DICE);
 		}
-<<<<<<< HEAD
-		System.out.println("The player reaches exactly 100 in "+ move +" moves");
-		System.out.println("The no of time the dice has been rolled :" + count);
-			
-	}	
-}
-
-
-
-
-
-=======
 	}
 }
->>>>>>> uc7
